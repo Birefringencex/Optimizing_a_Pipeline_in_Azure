d16bc915616cd66c1e811d5db424fecc36b15f1c
Gabi Lopez <30606674+Gabilopez1@users.noreply.github.com>
2020-11-15 23:44:41 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -9,9 +9,18 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
-from azureml.core.dataset import Dataset
 from azureml.core import Dataset, Datastore 
 from azureml.data.datapath import DataPath
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+from azureml.core.dataset import Dataset
+web_path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = Dataset.Tabular.from_delimited_files(path=web_path)
+
+run = Run.get_context()
 
 
 def clean_data(data):
@@ -43,18 +52,11 @@ def clean_data(data):
     return x_df, y_df
 
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-web_path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = Dataset.Tabular.from_delimited_files(path=web_path)
-
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 
 
 def main():
@@ -73,8 +75,6 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
-run = Run.get_context()
 
 if __name__ == '__main__':
     main()
